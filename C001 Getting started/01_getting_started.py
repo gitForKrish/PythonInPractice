166abd2dc3e48f80e55e361c4540ae96dfc0bd73
Krishnendu <krishnendu.mandal89@gmail.com>
2020-04-18 19:37:36 +0530

    Getting Started with Python

--- C001 Getting started/01_getting_started.py
+++ C001 Getting started/01_getting_started.py
@@ -1,7 +1,26 @@
-# To check installed python version
-# py --version
-# OR
-# python --version
+'''
+- Python is a 'dynamically typed' language, provides 'automatic memory management'
+and supports 'multiple programming paradigms' including object oriented, imperative, functional, and procedural programming.
 
+- Python 3.x is the current version under active development.
+
+- To execute python file: $python <file_name.py> OR $py <file_name.py>
+
+- Run commands as a string: $python -c 'print("Hello, World")'
+
+- Package Management 
+    - The PyPA (Python Packaging Authority) recommended tool for installing Python packages is PIP (Package Installer for Python).
+    - To install any package execute 
+        $pip install <the package name> (If pip is added to your PATH environment variables)
+        Or
+        $python -m pip install <the package name> (If pip is not added to your PATH environment variables)
+
+- To check installed python version
+    $py --version
+    Or
+    $python --version
+'''
+
+# Printing Greeting Messages
 print('Hello Python')
 print("Welcome to the world of python")
