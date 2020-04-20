79f9979eca1774292b31d4a4fa1d2058a66acb66
Krishnendu <krishnendu.mandal89@gmail.com>
2020-04-20 21:53:32 +0530

    Operator in progress

--- TeachingPython/03_operators.py
+++ TeachingPython/03_operators.py
@@ -1,4 +1,36 @@
-'''
-Python Operators
+# Python Operators
+# Arithmatic Operator
+a,b = 28,3
+print("Add=",a+b)
+print('Sub=',a-b)
+print("Mul=",a*b)
+print("Div=",a/b)
+print("Mod=",a%b)
+print("Floor=",a//b)
+print("Exp=",a**b)
 
-'''
+st = "hello"
+inputValue = 5
+print(st+str(inputValue)) # Type Conversion
+isValid = True
+print(str(isValid))
+
+print(True)
+# print(type(True==1))
+
+# issubclass()
+# isinstance()
+print(issubclass(bool, int))
+print(True==1)
+
+print(isinstance(True, bool))
+print(isinstance(4.5, int))
+print(isinstance(1, bool))
+print(isinstance(1, int))
+
+
+print(isinstance("\n", str))
+print(isinstance("5", int))
+print(isinstance(4.5e10, float))
+print(issubclass(int, float))
+
