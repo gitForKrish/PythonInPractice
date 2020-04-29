a1e45ba2c3be4779320470ca012a1f65cb4268b2
Krishnendu <krishnendu.mandal89@gmail.com>
2020-04-29 17:04:37 +0530

    Functional programming

--- TeachingPython/10_functionalProgram.py
+++ TeachingPython/10_functionalProgram.py
@@ -42,3 +42,35 @@ def showEven(x):
             yield i
 print(list(showEven(20)))
 print(list(showEven(200)))
+
+'''
+Assume you want to build two functions for discounting products on a website.
+Function number 1 is for student discount which discounts the current price to 10%.
+Function number 2 is for additional discount for regular buyers which discounts an additional 5% on the current student discounted price.
+Depending on the situation, we want to be able to apply both the discounts on the products.
+
+Design the above two mentioned functions and apply them both simultaneously on the price.
+'''
+def applyDiscountForStudent(x):
+    return x - x*0.1
+
+def applyAddiDiscountForRegular(func, arg):
+    studDisc = func(arg)
+    return studDisc - studDisc*0.05
+
+bookPrice = 240
+print("Price for student:", applyDiscountForStudent(bookPrice))
+print("Price for regular buyers:", applyAddiDiscountForRegular(applyDiscountForStudent, bookPrice))
+
+# Calculate the value of mathematical expression x*(x+5)^2 where x= 5 using lambda expression.
+myLambda = lambda x: x * (x + 5) **2
+print(myLambda(5)) 
+
+'''
+Consider a list in Python which includes prices of all the items in a store.
+Build a function to discount the price of all the products by 10%.
+Use map to apply the function to all the elements of the list so that all the product prices are discounted.
+'''
+priceList = [100,200,300,400,500]
+discountedPriceMap = map(lambda x: x-x*0.1, priceList)
+print(list(discountedPriceMap))
