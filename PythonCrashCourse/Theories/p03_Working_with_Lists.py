0ec344a2fae1b28c2f1b0f14fc7a3c4b0fed20e2
Krishnendu <krishnendu.mandal89@gmail.com>
2020-05-03 18:27:39 +0530

    Working with Lists

---
+++ PythonCrashCourse/Theories/p03_Working_with_Lists.py
@@ -0,0 +1,4 @@
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians:                              # Loop through the entire list
+    print(f"{magician.title()}, that was a great trick!")
