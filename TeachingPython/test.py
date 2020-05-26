12d29b13579d65598c7b04127a9b20f5ac113c20
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2020-05-26 16:48:04 +0530

    exercise

--- TeachingPython/test.py
+++ TeachingPython/test.py
@@ -1,22 +1,37 @@
-	# 9. List out  all the odd numbers from 1 to 100 using lists in Python.
-	# oddNumbers = [i for i in range(1,100,2)]
-	# print(oddNumbers)
-	# oddNumbers = []
-	# for i in range(1,100,2):
-	#     oddNumbers.append(i)
-	#     #print(oddNumbers) #[1], [1,3], [1,3,5] ... [99]
-	# print(oddNumbers)
+# 9. List out  all the odd numbers from 1 to 100 using lists in Python.
+# oddNumbers = [i for i in range(1,100,2)]
+# print(oddNumbers)
+# oddNumbers = []
+# for i in range(1,100,2):
+#     oddNumbers.append(i)
+#     #print(oddNumbers) #[1], [1,3], [1,3,5] ... [99]
+# print(oddNumbers)
 
 oddNumbers = []
-for i in range(1,100,2):
+for i in range(1, 100, 2):
     oddNumbers.append(i)
 print(oddNumbers)
 
 print()
-oddNumbers = list(range(1,100,2))
+oddNumbers = list(range(1, 100, 2))
 print(oddNumbers)
 
 print()
 # list comprehension
-oddNumbers = [i*i for i in range(1,100,2)]
+oddNumbers = [i*i for i in range(1, 100, 2)]
 print(oddNumbers)
+
+sentence = "This is a interview questions"
+# char_list = [*sentence]
+# print(char_list)
+
+# char_freq = {ch: sentence.count(ch) for ch in char_list}
+# print(char_freq)
+
+# highest_frequently_used = sorted(
+#     char_freq, key=lambda item: char_freq[item], reverse=True)[0]
+# print(highest_frequently_used)
+
+char_freq = {ch: sentence.count(ch) for ch in sentence}
+most_used = sorted(char_freq.items(), key=lambda kv: kv[1], reverse=True)[0]
+print(*most_used)
