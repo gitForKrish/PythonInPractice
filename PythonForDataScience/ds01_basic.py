3ce1fc3e1c3f1c2a44123650bc8a66bc03cc1242
Krishnendu <krishnendu.mandal89@gmail.com>
2020-06-23 09:03:58 +0530

    data science

--- PythonForDataScience/ds01_basic.py
+++ PythonForDataScience/ds01_basic.py
@@ -4,3 +4,26 @@ import keyword
 keywords = keyword.kwlist
 print(f"Total python keywords = {len(keywords)}")
 print(f"Here are the keyword list:\n{keywords}")
+
+print("*"*100)
+# Loops: While-else and For-else
+nums = [10, 12, 15]
+index = sum = 0
+while(index < len(nums)):
+    sum += nums[index]
+    # if nums[index] == 12:
+    #     break
+    index += 1
+else:
+    print('No more items left')
+print(f"Sum total = {sum}")
+
+sum = 0
+for num in nums:
+    sum += num
+    # if num == 12:
+    #     break
+else:
+    print('No more items left')
+print(f"Sum total = {sum}")
+print("*"*100)
